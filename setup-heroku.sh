89f9745f436df17d3b81d5370ce83e3a130b1ba3
Dor Polo <dorpolovincik@Dors-MacBook-Pro.local>
2022-06-18 20:06:50 +0300

    add sh helper

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -6,8 +6,11 @@ echo Writing permissons into ~/.netrc
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
-login dorpolo
-password $HEROKU_TOKEN
+  login dorpolo@gmail.com
+  password $HEROKU_TOKEN
+machine git.heroku.com
+  login dorpolo@gmail.com
+  password $HEROKU_TOKEN
 EOF
 chmod 600 ~/.netrc
 cat ~/.netrc
