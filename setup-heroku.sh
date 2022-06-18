c37dba63e2ea45ca3f4131373ea3cb251be086a6
Dor Polo <dorpolovincik@Dors-MacBook-Pro.local>
2022-06-18 19:34:06 +0300

    stages

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 HEROKU_TOKEN=$1
-HEROKU_APP_NAME=world-cup-22
 
 set -eu
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh 5 | sh
+
+echo heroku cli downloaded successfully
+
 cat > ~/.netrc << EOF
 machine api.heroku.com
-login dorpolo
+login dorpolo@gmail.com
 password $HEROKU_TOKEN
 EOF
 chmod 600 ~/.netrc
+echo ~/.netrc updated successfully
