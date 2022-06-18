d52853a23eb6dc931d2dc8b5adba15d423091b6b
Dor Polo <dorpolovincik@Dors-MacBook-Pro.local>
2022-06-18 18:33:31 +0300

    insert ENV VARS

--- world_cup/views.py
+++ world_cup/views.py
@@ -1,6 +1,10 @@
+import os
+
 from django.http import JsonResponse
+
+pg: str = os.getenv('PG_PASS') if os.getenv('PG_PASS') else "key was not fount"
 
 
 def ping(request):
-    data = {'ping': 'pong!'}
+    data = {'ping': pg}
     return JsonResponse(data)
